a3ae1682e8b1756cea4b15797971e60a92f66849
Massimiliano Pesce <massimiliano.pesce@gmail.com>
2017-07-02 19:30:19 +0200

    First version of setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mgl2d',
+    version='0.1.0',
+    description='Simple 2D game library // PySDL2 + modern OpenGL',
+    author='maxfish',
+    url='https://github.com/maxfish/mgl2d',
+    dependency_links=['git+https://github.com/maxfish/PyTMX.git#egg=PyTMX-3.20.17'],
+    install_requires=[
+        'numpy == 1.13.0',
+        'Pillow == 4.1.1',
+        'PyOpenGL == 3.1.0',
+        'PySDL2 == 0.9.5',
+        'PyTMX == 3.20.17'
+    ],
+    packages=find_packages()
+)
