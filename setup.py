6dcdb67253af1cea2c0addce6bdd6ce26c8ef06a
Massimiliano Pesce <massimiliano.pesce@gmail.com>
2017-07-12 19:49:03 +0200

    PyTMX dependency is temporarily handled manually

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     description='Simple 2D game library // PySDL2 + modern OpenGL',
     author='maxfish',
     url='https://github.com/maxfish/mgl2d',
-    dependency_links=['git+https://github.com/maxfish/PyTMX.git#egg=PyTMX-3.20.17'],
     install_requires=[
         'numpy == 1.13.0',
         'Pillow == 4.1.1',
         'PyOpenGL == 3.1.0',
         'PySDL2 == 0.9.5',
-        'PyTMX == 3.20.17'
+        # 'PyTMX == 3.20.17'
+        # Manual install a fork with fixes from git+https://github.com/maxfish/PyTMX.git#egg=PyTMX
     ],
     packages=find_packages()
 )
