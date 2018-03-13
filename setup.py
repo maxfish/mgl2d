dfd068a7f69937559ed1a64309c55f52076a362c
Massimiliano Pesce <massimiliano.pesce@gmail.com>
2018-03-13 21:27:21 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mgl2d',
-    version='0.1.2',
+    version='0.1.3',
     description='Simple 2D game library // PySDL2 + modern OpenGL',
     author='maxfish',
     url='https://github.com/maxfish/mgl2d',
