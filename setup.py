cb801707e1744a8b0e1e92549f11494eafbb0569
Massimiliano Pesce <massimiliano.pesce@gmail.com>
2018-03-18 08:27:50 +0100

    bumped version to 0.9.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ LONGDESCRIPTION = 'See the README.md file on GitHub for more information.'
 URL = 'https://github.com/maxfish/mgl2d'
 EMAIL = 'massimiliano.pesce@gmail.com'
 AUTHOR = 'Massimiliano Pesce'
-VERSION = '0.9.1'
+VERSION = '0.9.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
